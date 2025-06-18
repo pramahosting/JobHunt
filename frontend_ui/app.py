9ffb2d1b514eb026887f8ef1c2751da1abd27d8c
pramahosting <pramahosting@gmail.com>
2025-06-19 08:56:00 +1000

    Update app.py

--- frontend_ui/app.py
+++ frontend_ui/app.py
@@ -22,7 +22,7 @@ st.title("JobHunt Agent – Smart Job Search")
 
 # === Upload Resume (Box) ===
 st.markdown("""
-<div style="border: 2px solid #D3D3D3; border-radius: 10px; padding: 20px; margin-bottom: 20px;">
+# <div style="border: 2px solid #D3D3D3; border-radius: 10px; padding: 20px; margin-bottom: 20px;">
 <h4>Upload Resume</h4>
 """, unsafe_allow_html=True)
 
